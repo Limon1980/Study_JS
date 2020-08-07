77b611bef8f97b493ad7b57f232033864cbee54a
Limon1980 <tarkalmob@yandex.ru>
2020-08-07 19:03:45 +1000

    Урок 2 основное задание завершен

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,16 @@ let addExpenses = 'жкх, интернет, машина';
 let deposit = true;
 let mission = 6e6;
 let period = 12;
+let budgetDay = money / 30;
 
 
-console.log(period);
-console.log("Hello World!");
-console.log("Я закончил первый урок!");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+addExpenses
+console.log('addExpenses: ', addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log((addExpenses.toLocaleUpperCase()).split(','));
+console.log('budgetDay: ', budgetDay);
